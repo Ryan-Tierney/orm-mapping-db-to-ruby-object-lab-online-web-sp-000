3a0254f271279dddeddb15669ddd490b0a794811
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-19 03:20:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students 
-      WHERE grade = all_students_in_grade_9
+      WHERE grade = 9
       SQL
       
       DB[:conn].execute(sql).map do |row|
